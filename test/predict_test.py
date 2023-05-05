b3be0017ab86019a2df0cf0253cc34573dab278b
Bastien <b.burdalo@gmail.com>
2023-05-05 17:03:44 +0200

    update model & test accuracy

--- test/predict_test.py
+++ test/predict_test.py
@@ -1,17 +1,17 @@
 import pytest
 import os.path
 
-from app.predict.predict import get_predictions
+from app.predict.predict import get_predictions, get_accuracy
 
 
 def test_get_predictions():
     """Prédiction à partir d'un fichier physique"""
-    response = get_predictions(os.path.join("test", "106026.jpg"), 1)
-    assert response[0]['name'] == 'Galerina marginata'
+    response = get_predictions(os.path.join("test", "1494823.jpg"), 1)
+    assert response[0]['name'] == 'Pleurotus pulmonarius'
 
     """Prédiction à partir d'une URL"""
-    response = get_predictions("https://images.mushroomobserver.org/320/1552315.jpg", 1)
-    assert response[0]['name'] == 'Amanita phalloides'
+    response = get_predictions("https://images.mushroomobserver.org/320/1536252.jpg", 1)
+    assert response[0]['name'] == 'Amanita muscaria'
     
 def test_get_predictions_raises_exception_on_non_picture_argument():
     """Exception pour un format de fichier non pris en charge"""
@@ -27,3 +27,7 @@ def test_get_predictions_raises_exception_on_non_int_argument():
     """Exception pour un nombre de prédictions non entier"""
     with pytest.raises(TypeError):
         get_predictions(os.path.join("test", "106026.jpg"), '4')
+
+def test_get_accuracy():
+    response = get_accuracy()
+    assert response >= 0.7
