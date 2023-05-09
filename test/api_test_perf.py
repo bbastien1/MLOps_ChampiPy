b24a2404441711fad07220d2d5d6b244abc046f6
Bastien <b.burdalo@gmail.com>
2023-05-09 12:47:09 +0200

    workflow check api perfs

---
+++ test/api_test_perf.py
@@ -0,0 +1,37 @@
+import sys
+import os
+import pytest
+from httpx import AsyncClient, BasicAuth
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from app.api.main import api
+
+@pytest.mark.anyio
+async def test_status():
+    async with AsyncClient(app=api, base_url="http://test") as ac:
+        response = await ac.get("/status")
+    assert response.json() == True
+
+
+@pytest.mark.anyio
+async def test_db_connex():
+    async with AsyncClient(app=api, base_url="http://test") as ac:
+        response = await ac.get("/dbconnex")
+    assert response.json() == True
+
+@pytest.mark.anyio
+async def test_accuracy():
+    async with AsyncClient(app=api, base_url="http://test") as ac:
+        auth = BasicAuth("admin", "admin")
+        response = await ac.get("/accuracy", auth=auth)
+    assert response.json() >= sys.argv[1] 
+
+
+@pytest.mark.anyio
+async def test_accuracy():
+    async with AsyncClient(app=api, base_url="http://test") as ac:
+        auth = BasicAuth("admin", "admin")
+        response = await ac.get("/nb_new_img", auth=auth)
+    assert response.json() < sys.argv[2] 
